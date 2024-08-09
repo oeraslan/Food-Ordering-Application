295e38100b9348a8f4a0e354a5a461fb842486b0
Ömer Eraslan <eraslan.omer@hotmail.com>
2024-08-09 19:58:33 +0300

    application development started

---
+++ src/test/java/com/oeraslan/foodorderingapplication/FoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oeraslan.foodorderingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
