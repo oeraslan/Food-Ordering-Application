98a277fc1b817924d3d1e35d98e37ca875815888
Ömer Eraslan <eraslan.omer@hotmail.com>
2024-08-10 19:43:18 +0300

    Category and Status enums added

---
+++ src/main/java/com/oeraslan/foodorderingapplication/enums/Category.java
@@ -0,0 +1,10 @@
+package com.oeraslan.foodorderingapplication.enums;
+
+public enum Category {
+    DRINKS,
+    MAIN_COURSE,
+    DESSERT,
+    SALAD,
+    SNACKS,
+    SOUP
+}
---
+++ src/main/java/com/oeraslan/foodorderingapplication/enums/Status.java
@@ -0,0 +1,9 @@
+package com.oeraslan.foodorderingapplication.enums;
+
+public enum Status {
+    PREPARING,
+    READY,
+    DELIVERED,
+    CANCELLED,
+    PAID
+}
