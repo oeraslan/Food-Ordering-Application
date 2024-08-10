7a6c5c885a41c64d3b8dfc7c4b1c4e4b02aac18b
Ömer Eraslan <eraslan.omer@hotmail.com>
2024-08-10 21:04:35 +0300

    Get active orders sql added

--- src/main/java/com/oeraslan/foodorderingapplication/repository/OrderRepository.java
+++ src/main/java/com/oeraslan/foodorderingapplication/repository/OrderRepository.java
@@ -2,8 +2,15 @@ package com.oeraslan.foodorderingapplication.repository;
 
 import com.oeraslan.foodorderingapplication.repository.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    @Query(value = "SELECT * FROM orders WHERE status IN ('RECEIVED', 'PREPARING', 'READY') AND deleted = false", nativeQuery = true)
+    public List<Order> getActiveOrders();
+
 }
